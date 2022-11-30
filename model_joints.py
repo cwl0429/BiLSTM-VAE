07e7de09d505ce52e09bf10fd4f09efdf3f422fe
cwl0429 <chunweilai29@gmail.com>
2022-11-30 17:25:57 +0800

    Refactor: encapsulate model joints definition
    class Joindef provides methods for concate motions

---
+++ model_joints.py
@@ -0,0 +1,55 @@
+import numpy as np
+import torch
+class JointDef:
+    def cat_torch(self, part, data):
+        if part == 'torso':
+            part_data = torch.cat((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'leftarm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='rightarm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'leftleg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'rightleg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        return part_data
+
+    def cat_numpy(self, part, data):
+        if part == 'torso':
+            part_data = np.concatenate((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'leftarm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='rightarm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'leftleg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'rightleg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        return part_data
+
+class JointDefPrev:
+    def cat_torch(self, part, data):
+        if part == 'torso':
+            part_data = torch.cat((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'leftarm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='rightarm':
+            part_data = torch.cat((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'leftleg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'rightleg':
+            part_data = torch.cat((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        return part_data
+
+    def cat_numpy(self, part, data):
+        if part == 'torso':
+            part_data = np.concatenate((data[:, 0:9], data[:, 15:18], data[:, 24:30], data[:, 36:39]), axis=1)
+        elif part == 'leftarm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 18:24]), axis=1)
+        elif part =='rightarm':
+            part_data = np.concatenate((data[:, 3:9], data[:, 15:18], data[:, 24:27], data[:, 9:15]), axis=1)
+        elif part == 'leftleg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 39:45]), axis=1)
+        elif part == 'rightleg':
+            part_data = np.concatenate((data[:, 3:6], data[:, 24:30], data[:, 36:39], data[:, 30:36]), axis=1)
+        return part_data
